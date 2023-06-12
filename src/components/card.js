5afdb56e2a96b2d5bca8465b1f8813e12bc4bb6d
Alme <josealejandrogilmendez08@gmail.com>
2023-06-12 18:14:04 -0400

    add card click handle and change the div tag to button

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 
 function Card(props) {
-  const { pok } = props;
+  const { pok, handleCardClick } = props;
   return (
-    <div>
+    <button type="button" onClick={() => handleCardClick(pok.id)}>
       <img src={pok.sprites.front_default} alt={pok.name} />
       <p>{pok.name}</p>
-    </div>
+    </button>
   );
 }
 Card.propTypes = {
   /* eslint-disable react/forbid-prop-types */
   pok: PropTypes.object.isRequired,
+  handleCardClick: PropTypes.func.isRequired,
   /* eslint-enable react/forbid-prop-types */
 };
 
